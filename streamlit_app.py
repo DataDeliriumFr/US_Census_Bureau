191b96907e1bab12c6a563a9d0c6bd9f29a52513
DataDeliriumFr <67931147+DataDeliriumFr@users.noreply.github.com>
2023-03-05 12:44:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import io
 
+favicon = """
+https://www.google.com/url?sa=i&url=https%3A%2F%2Fthenounproject.
+com%2Fbrowse%2Ficons%2Fterm%2Fcensus%2F&psig=AOvVaw3yTDTEHm_8LY-Evos9Qc2f&ust=
+1678097795774000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKCpis7HxP0CFQAAAAAdAAAAABAP
+"""
+st.set_page_config(
+    page_title="U.S. Census Bureau App",
+    page_icon=favicon,
+    layout="centered"
+)
 
 st.image("https://www.census.gov/mycd/application/images/census_doc_logo.png")
 st.subheader("Interactive Dashboard : Yearly Birth Rate per Country")
@@ -17,13 +27,13 @@ for country in countries:
     country_list.append(name)
 
 country = st.selectbox("Select country to visualize data :", country_list)
+st.write("")
 
 if country:
     try:
         country_code = countries.get(name=country).alpha_2
         url = f"https://api.census.gov/data/timeseries/idb/1year?get=NAME,AGE,POP,AREA_KM2&GENC={country_code}&YR=2021&SEX=0"
-        response = requests.get(url)
-        response = response.json()
+        response = requests.get(url).json()
         df = pd.DataFrame(response, columns=response[0])
 
         df1 = df.drop(0)
@@ -33,8 +43,19 @@ if country:
         df2 = pd.DataFrame(columns=["Year", "Number of Births"])
         df2["Year"] = 2021 - df1["AGE"]
         df2["Number of Births"] = df1["POP"]
-        df2.style.format({"Number of Births": lambda x :"{:,}".format(x)})
+        df2.style.format({"Number of Births": lambda x: "{:,}".format(x)})
         df2 = df2.set_index("Year")
+
+        col1, col2, col3, col4 = st.columns(4)
+        with col2:
+            st.metric(label="Min. Year",
+                      value=df2["Number of Births"].idxmin(),
+                      delta=None)
+        with col4:
+            st.metric(label="Max. Year",
+                      value=df2["Number of Births"].idxmax(),
+                      delta=None)
+        st.write("")
 
         plt.figure(figsize=(20, 7))
         sns.barplot(data=df2, x=df2.index, y=df2["Number of Births"], color="#226094")
@@ -47,13 +68,19 @@ if country:
 
         st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
-        
-        st.download_button(label="Download Graph", data=img, file_name=f"U.S. Census Bureau - Yearly Birth Rate - {country}.png", mime="png")
-        
-        with st.expander(f"View data of {country}"):
-            st.dataframe(df2)
 
-    except (AttributeError):
+        st.download_button(label="Download Graph",
+                            data=img,
+                            file_name=f"U.S. Census Bureau - Yearly Birth Rate - {country}.png",
+                            mime="png"
+                            )
+
+        st.write("")
+        with st.expander(f"View data of {country}"):
+            st.dataframe(data=df2,
+                         use_container_width=True)
+
+    except AttributeError:
         st.write("")
     except json.decoder.JSONDecodeError:
         st.error("Country not referenced in U.S. Census Bureau database. Try another country.")
