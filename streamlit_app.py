fbdc23158ac8fe4c4633ad467268fa7a891e7131
DataDeliriumFr <67931147+DataDeliriumFr@users.noreply.github.com>
2023-03-12 14:07:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,22 @@ from pycountry import countries
 import requests
 import json
 import pandas as pd
+from st_aggrid import AgGrid
 import seaborn as sns
 import matplotlib.pyplot as plt
 import io
 
+# Set app settings
 st.set_page_config(
     page_title="U.S. Census Bureau App",
     layout="centered"
 )
 
+# App header
 st.image("https://www.census.gov/mycd/application/images/census_doc_logo.png")
-st.subheader("Interactive Dashboard : Yearly Birth Rate per Country")
+st.subheader("Interactive Dashboard : Yearly Birth Rate (1921-2021)")
 
+# Create list of countries to select from
 country_list = [""]
 for country in countries:
     name = country.name
@@ -22,67 +26,91 @@ for country in countries:
 
 country = st.selectbox("Select country to visualize data :", country_list)
 
+# Insert space
 st.write("")
 
+# Once country is selected...
 if country:
     try:
+        # Fetch data
         country_code = countries.get(name=country).alpha_2
         url = f"https://api.census.gov/data/timeseries/idb/1year?get=NAME,AGE,POP,AREA_KM2&GENC={country_code}&YR=2021&SEX=0"
         response = requests.get(url).json()
         df = pd.DataFrame(response, columns=response[0])
+        df.drop(0, inplace=True)
 
-        df1 = df.drop(0)
-        df1["AGE"] = df1["AGE"].astype("int")
-        df1["POP"] = df1["POP"].astype("int")
+        # Wrangle data
+        df1 = pd.DataFrame(columns=["Year", "Number of Births", "% Rate"])
+        df1["Year"] = 2021 - df["AGE"].astype("int")
+        df1["Number of Births"] = df["POP"].astype("int")
+        df1["% Rate"] = round(df1["Number of Births"].pct_change(periods=-1), 2)
 
-        df2 = pd.DataFrame(columns=["Year", "Number of Births", "TempCalculation", "Progression"])
-        df2["Year"] = 2021 - df1["AGE"]
-        df2["Number of Births"] = df1["POP"]
-        df2["TempCalculation"] = df2["Number of Births"].shift(-1)
-        df2["Progression"] = round((df2["Number of Births"] - df2["TempCalculation"]) / df2["TempCalculation"], 2)
-        df2 = df2.drop("TempCalculation", axis=1)
-        df2.style.format({"Number of Births": lambda x: "{:,}".format(x)})
-        df2 = df2.set_index("Year")
-
-        col1, col2, col3, col4 = st.columns(4)
+        # Select time range
+        col1, col2 = st.columns(2)
+        with col1:
+            index_year_1921 = len(df1["Year"]) - 1
+            start_year = st.selectbox("Select start year :", df1["Year"], index=index_year_1921)
         with col2:
-            st.metric(label="Year Min. Births",
-                      value=df2["Number of Births"].idxmin(),
-                      delta=None)
-        with col4:
-            st.metric(label="Year Max. Births",
-                      value=df2["Number of Births"].idxmax(),
-                      delta=None)
+            end_year = st.selectbox("Select end year :", df1["Year"])
         st.write("")
 
+        # Filter dataframe based on selected time range
+        df1 = df1.loc[(df1["Year"] >= start_year) & (df1["Year"] <= end_year)]
+
+        # Display metrics
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col2:
+            min_rate = df1["% Rate"].min()
+            min_index = df1["% Rate"].idxmin()
+            st.metric(label="Min. Rate Year",
+                      value=df1.loc[min_index, "Year"],
+                      delta=f"{min_rate:.1%}"
+                      )
+        with col3:
+            max_rate = df1["% Rate"].max()
+            max_index = df1["% Rate"].idxmax()
+            st.metric(label="Max. Rate Year",
+                      value=df1.loc[max_index, "Year"],
+                      delta=f"{max_rate:.1%}"
+                      )
+        with col4:
+            average_rate = df1["% Rate"].mean()
+            st.metric(label="Average Rate",
+                      value=f"{average_rate:.1%}"
+                      )
+
+        # Insert space
+        st.write("")
+
+        # Generate and display graph
         plt.figure(figsize=(20, 7))
-        sns.barplot(data=df2, x=df2.index, y=df2["Number of Births"], color="#226094")
+        sns.barplot(data=df1, x=df1["Year"], y=df1["Number of Births"], color="#226094")
         plt.xticks(rotation=90)
-        plt.title(f"U.S. Census Bureau - Yearly Birth Rate of {country}")
+        plt.title(f"U.S. Census Bureau - Yearly Birth Rate\n{country} ({start_year}-{end_year})")
         plt.xlabel("")
         plt.show()
-        img = io.BytesIO()
-        plt.savefig(img, format='png')
+        image = io.BytesIO()
+        plt.savefig(image, format='png')
 
         st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
 
+        # Display button to download graph
         st.download_button(label="Download Graph",
-                           data=img,
-                           file_name=f"U.S. Census Bureau - Yearly Birth Rate - {country}.png",
+                           data=image,
+                           file_name=f"U.S. Census Bureau - Yearly Birth Rate - {country} ({start_year}-{end_year}).png",
                            mime="png"
                            )
 
+        # Insert space
         st.write("")
 
-        with st.expander(f"View data of {country}"):
-            # Style bars inside cells of Number of Births column
-            # df2_style = df2.style.bar(subset=["Number of Births"], axis=0, cmap="Blues")
-            # To get styling to work with Streamlit 1.10.0, convert the styled dataframe to HTML
-            # st.write(df2_style.to_html(escape=False), unsafe_allow_html=True)
-            st.dataframe(data=df2, use_container_width=True)
+        # Copy dataframe, reformat data, display data
+        df2 = df1.copy()
+        df2["% Rate"] = df2["% Rate"] * 100
 
-    except AttributeError:
-        st.write("")
+        with st.expander(f"Interactive table of {country}"):
+            AgGrid(df2)
+
     except json.decoder.JSONDecodeError:
         st.error("Country not referenced in U.S. Census Bureau database. Try another country.")
